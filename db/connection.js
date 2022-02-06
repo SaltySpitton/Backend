23c5cbe157cffeced844adb0121274a6ae141ade
ssallquiz <sallquist.sylvie@gmail.com>
2022-02-06 00:46:03 -0800

    added user seeds

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const mongoURI = process.env.NODE_ENV === 'production' 
+? process.env.DB_URL
+: process.env.MONGO_URI
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+.then(instance => console.log(instance.connections[0].name))
+.catch(err => console.error(err))
+
+module.exports = mongoose
