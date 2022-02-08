477c0e8692766eb74cf3614f2c921551ee938a03
ssallquiz <sallquist.sylvie@gmail.com>
2022-02-08 14:30:37 -0800

    merging data with Ali back

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 4200
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-
+app.get('/', (req,res) => {
+    console.log('hello from devs to you')
+})
 
 
 app.listen(port, () => {
