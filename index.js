4d833fea0512618bad36db03ed497deb8f64173e
alialhakeem <ali.b.alhakim@gmail.com>
2022-02-08 19:29:45 -0700

    seeded  Q & A

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 4200
+const express = require("express");
+const app = express();
+const port = 4200;
+const methodOverride = require("method-override");
+const session = require("express-session");
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Require All Controllers
 
+const questionsControllers = require("./controllers/questions");
 
+// App use
+app.use(questionsControllers);
+
+const routeHit = (req, res, next) => {
+  console.log("A new route was just hit");
+  next();
+};
+app.use(routeHit);
 
 app.listen(port, () => {
-    console.log(`🎉🎊' Port is connected at ${port}`)
-})
+  console.log(`🎉🎊' Port is connected at ${port}`);
+});
--- models/questions.js
+++ models/questions.js
@@ -1,51 +1,44 @@
-const mongoose = require('../db/connection')
+const mongoose = require("../db/connection");
 
 const questionSchema = new mongoose.Schema({
-    date:{
-        type:String, 
-        required: true
-    },
-    tags: {
-        type: String,
-        enum: [
-          'html',
-          'css',
-          'java',
-          'javascript',
-          'node.js',
-          'mongoose',
-          'mongodb',
-          'react',
-          'jquery',
-          "jsx",
-          'python',
-          'json',
-          'django',
-          'ejs',
-          'other',
-        ],
-        require: true,
-    },
-    title: {
-        type: String, 
-        required: true
-    }, 
-    body: {
-        type: String, 
-        required: true
-    }, 
-    votes: {
-        type: Number, 
-        default: 0
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User'
-    }
+  date: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    enum: [
+      "html",
+      "css",
+      "java",
+      "javascript",
+      "node.js",
+      "mongoose",
+      "mongodb",
+      "react",
+      "jquery",
+      "jsx",
+      "python",
+      "json",
+      "django",
+      "ejs",
+      "other",
+    ],
+    require: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  votes: {
+    type: Number,
+    default: 0,
+  },
+});
 
-})
-
-const Question = mongoose.model('Question', questionSchema)
-module.exports = Question
-
-
+const Question = mongoose.model("Question", questionSchema);
+module.exports = Question;
