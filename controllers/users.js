07d3144e5f0c91cef2a7d0dc42a97f7698f697df
dlongo <dlongodesign@gmail.com>
2022-02-12 15:54:03 -0500

    :bug: fix get user route to get user in session

--- controllers/users.js
+++ controllers/users.js
@@ -1,32 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const Question = require('../models/questions');
-const Answer = require('../models/answers');
-const User = require('../models/users');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const Question = require("../models/questions");
+const Answer = require("../models/answers");
+const User = require("../models/users");
 
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
     if (err) throw err;
-    if (!user) res.send('No User Exists');
+    if (!user) res.send("No User Exists");
     else {
       req.logIn(user, (err) => {
         if (err) throw err;
-        res.send('Successfully Authenticated');
+        res.send("Successfully Authenticated");
         console.log(req.user);
       });
     }
   })(req, res, next);
 });
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   User.findOne({ username: req.body.username }, async (err, doc) => {
-    if (err){
-        await res.status(400).json({error: error.message})
-    }
-    if (doc){
-        await res.status(500).json({error: 'Unable to register'})
-    }
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");
     if (!doc) {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
@@ -36,36 +32,22 @@ router.post('/register', (req, res) => {
         email: req.body.email,
       });
       await newUser.save();
-      res.send('User Created');
+      res.send("User Created");
+      // res.redirect('http://localhost:3000/questions');
     }
   });
 });
-
-
-
-router.get('/', async(req, res, next) => {
-  try{
-      const user = await User.find({})
-      user ?
-      res.status(200).json(user) :
-      res.status(400).json({error: error.message})
-  }catch(err){
-    next(err)
-  }
-  // console.log(req.user);
-  // res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
+router.get("/", (req, res) => {
+  console.log(req.user);
+  res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
 });
 
-
-router.get('/logout', async(req,res,next) => {
-    try{
-        const logout = await req.logout()
-        res.status(200).json({message: 'User Succesfully Logged Out'})
-
-    }catch(err){
-      next(err)
-    }    
-})
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.send('user logged out')
+  // res.redirect('http://localhost:3000/questions');
+  // console.log("user logged out")
+});
 
 //EDIT USER 
 //DELETE USER
